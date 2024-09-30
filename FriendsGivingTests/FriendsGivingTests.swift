5b944760648a075e489f707ce473fec210647805
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-09-30 10:59:50 +1000

    Initial Commit

---
+++ FriendsGivingTests/FriendsGivingTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingTests.swift
+//  FriendsGivingTests
+//
+//  Created by GuitarLearnerJas on 30/9/2024.
+//
+
+import Testing
+@testable import FriendsGiving
+
+struct FriendsGivingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
